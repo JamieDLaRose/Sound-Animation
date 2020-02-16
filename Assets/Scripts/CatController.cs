83e2f8b4bdf55a7c6a7a3de72f836bee1f4ae92c
JamieDLaRose <52182231+JamieDLaRose@users.noreply.github.com>
2020-02-16 12:17:07 -0500

    Initial commit

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+     //to quit the game hit escape
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
